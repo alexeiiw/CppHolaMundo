e981335370b4152e473bf12b3d369332ebf24a9c
alexeiiw <alexeiiw@gmail.com>
2020-02-09 22:59:04 +0000

    Archivo de hoy

---
+++ Prueba.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+// Define namespace
+using namespace std;
+ 
+// Metodo principal
+int main() {
+ 
+    // Hola Mundo
+    cout<<"Hola Mundo 3"<<endl;
+ 
+    return 0;
+ 
+}
