89024038e54db73167908b8408c114ded387bc59
alexeiiw <alexeiiw@gmail.com>
2020-02-09 23:45:01 +0000

    Cambios de hoy

---
+++ Prueba2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+// Define namespace
+using namespace std;
+ 
+// Metodo principal
+int main() {
+ 
+    // Hola Mundo
+    cout<<"Hola Mundo 3 Programacion"<<endl;
+ 
+    return 0;
+ 
+}
