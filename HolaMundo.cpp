cc42c10bfa7541cdf190c9a4b9e416bfca668a68
Alex García <alexeiiw@gmail.com>
2022-07-03 21:02:47 +0000

    Cambios de hoy

--- HolaMundo.cpp
+++ HolaMundo.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main() {
  
     // Hola Mundo
-    cout<<"Hola Mundo 3 desde codespaces"<<endl;
+    cout<<"Hola Mundo 3 desde codespaces y desde linux"<<endl;
  
     return 0;
 }
